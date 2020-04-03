7348e61ea024dcc7c36116b7930eaa760b325735
Nandita Venkat <nanditavenkat@gmail.com>
2020-04-03 19:05:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,10 +34,14 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
+  num=["am","unnu","rendu","moonu","nalu"]
+  num[0]
   
 end
 
 def retrieve_last_element_from_array(array)
+  num=["am","unnu","rendu","moonu","nalu"]
+  num[-1]
   
 end
 
