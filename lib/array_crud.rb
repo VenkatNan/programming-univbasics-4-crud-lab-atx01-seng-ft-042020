680ec7562a6076adeee89194f2ec96c86a85337d
Nandita Venkat <nanditavenkat@gmail.com>
2020-04-03 19:06:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -40,7 +40,7 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  num=["arrays!","unnu","rendu","moonu","nalu"]
+  num=["opopo","unnu","rendu","moonu","nalu","arrays!"]
   num[-1]
   
 end
