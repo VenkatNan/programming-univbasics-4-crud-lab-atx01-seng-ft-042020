a6dad4b6e4d389663fa22cded0ae39397a245ee0
Nandita Venkat <nanditavenkat@gmail.com>
2020-04-03 19:11:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,30 +11,26 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  num=["unnu","rendu","moonu","nalu"]
-  num.unshift("wow")
+
+  num.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  num=["unnu","rendu","moonu","nalu","arrays!"]
-  bub=num.pop
+  bub=array.pop
   bub
 end
 
 def remove_element_from_start_of_array(array)
-  num=["wow","unnu","rendu","moonu","nalu"]
-  tab = num.shift
+  tab = array.shift
   tab
 end
 
 def retrieve_element_from_index(array, index_number)
-  num=["am","unnu","rendu","moonu","nalu"]
-  num[0]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  num=["wow","unnu","rendu","moonu","nalu"]
-  num[0]
+ array[0]
   
 end
 
